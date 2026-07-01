669b99028e35309553f6c244372cfd39982080f4
Goolies <chadprobert1@gmail.com>
2026-07-01 13:58:01 +0200

    Update admin-main.jsx

--- src/admin-main.jsx
+++ src/admin-main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Admin from './Admin.jsx';
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Admin />
+    <App />
   </React.StrictMode>
 );
